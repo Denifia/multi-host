2f5429f183b1799cb71fd39403e218fb606f1625
Denifia <mr.l.wale@gmail.com>
2025-03-22 20:42:03 +0800

    spawn new process

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::process::{Command, Stdio};
+
 use iced::widget;
 
 struct Counter {
     // This will be our state of the counter app
     // a.k.a the current count value
     count: i32,
+    text: String
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -18,7 +21,7 @@ impl Counter {
     fn new() -> Self {
         // initialize the counter struct
         // with count value as 0.
-        Self { count: 0 }
+        Self { count: 0, text: String::from("blank") }
     }
 
     fn update(&mut self, message: Message) -> iced::Task<Message> {
@@ -35,7 +38,8 @@ impl Counter {
         let row = widget::row![
             widget::button("-").on_press(Message::DecrementCount),
             widget::text(self.count),
-            widget::button("+").on_press(Message::IncrementCount)
+            widget::button("+").on_press(Message::IncrementCount),
+            widget::text(self.text.clone()).height(200).width(200)
         ];
         widget::container(row)
             .center_x(iced::Length::Fill)
@@ -53,6 +57,16 @@ impl Default for Counter {
 }
 
 fn main() -> Result<(), iced::Error> {
+    let child = Command::new("cmd")
+        //.arg("-Wait C:/Users/mrlwa/source/repos/multi-host/demo.log")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+
+    let output = child
+        .wait_with_output()
+        .expect("failed to wait on child");
+
     // run the app from main function
     iced::application("Counter Example", Counter::update, Counter::view).run_with(|| (Counter::new(), iced::Task::none()))
 }
