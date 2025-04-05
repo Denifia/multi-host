428692eff4f6e15fd41cb626a8abeb4d5359dd8c
Denifia <mr.l.wale@gmail.com>
2025-04-05 15:09:36 +0800

    refactor example-project
    Went looking for argument parsing and noticed that it could be a
    little cleaner than it was and only took a few mins to change/test.

--- example-process/src/main.rs
+++ example-process/src/main.rs
@@ -7,32 +7,38 @@ fn main() {
     let should_run_forever = args.iter().any(|s| s == "--forever");
 
     match (should_throw, should_run_forever) {
-        (false, true) => run_forever(),
+        (false, true) => {
+            println!("I'm running forever!");
+            run_forever()
+        }
         (true, _) => {
             println!("I'm running but will throw!");
             run_and_stop();
             panic!("bing bong!");
-        },
+        }
         _ => {
             println!("I'm running!");
             run_and_stop()
-        },
+        }
     }
 }
 
 fn run_and_stop() {
     for i in 1..5 {
-        println!("{}: Hello, world @ {:?}", i, datetime::Instant::now());
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        print_slowly(i);
     }
 }
 
 fn run_forever() {
-    println!("I'm running forever!");
     let mut i = 0;
     loop {
-        println!("{}: Hello, world @ {:?}", i, datetime::Instant::now());
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        print_slowly(i);
         i += 1;
     }
 }
+
+fn print_slowly(i: i32) {
+    println!("{}: Hello, world @ {:?}", i, datetime::Instant::now());
+    std::thread::sleep(std::time::Duration::from_secs(1));
+}
+
